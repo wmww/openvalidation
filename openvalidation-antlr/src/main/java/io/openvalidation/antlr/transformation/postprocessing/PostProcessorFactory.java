30d2284a0de9f6b4433d05e56e0ab1ee05da3b23
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2020-01-15 09:36:49 +0100

    added failing test for where function on simple type array

--- openvalidation-antlr/src/main/java/io/openvalidation/antlr/transformation/postprocessing/PostProcessorFactory.java
+++ openvalidation-antlr/src/main/java/io/openvalidation/antlr/transformation/postprocessing/PostProcessorFactory.java
@@ -49,6 +49,7 @@ public class PostProcessorFactory {
     _postprocessors.add(new PostModelImplicitBoolVariables());
     _postprocessors.add(new PostModelMissingRightOperandResolver());
     _postprocessors.add(new PostConditionSingleOperand());
+    _postprocessors.add(new PostModelWhereOnSimpleTypeResolver());
   }
 
   public List<PostProcessorBase> create(ASTItem item) {
